8f98334f5abe4e5e7f6f9b71cac4de0b89b7269d
himuro <himuro.keep.alive@gmail.com>
2015-07-09 08:28:19 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# wheel_reloader
+Reloads page by mouse wheel on page bottom  
+マウスホイールでリロードしちゃう
+
+Reloads the page by wheel down on page bottom some times (4 times by default).  
+When reload the page scrolls to page top by option.
+
+ページ末尾でホイールを設定回数分(初期値は4)下に回すとページをリロードします  
+リロード時にページ先頭に戻るようにもできます  
+
+Please customize the Include(Exclude) Pages in the Script Preferences by yourself.  
+お好みで実行するページ/実行しないページをカスタマイズしてください。
---
+++ wheel_reloader.user.js
@@ -0,0 +1,80 @@
+// ==UserScript==
+// @name           wheel reloader
+// @namespace      https://github.com/himuro-majika
+// @description    Reloads page by mouse wheel on page bottom
+// @description    マウスホイールでリロードしちゃう
+// @include        *
+// ==/UserScript==
+(function () {
+/*
+ステータスバーへの表示はFirefox本体のオプションから
+コンテンツ > JavaScriptの詳細設定 > ステータスバーのテキストを変更する
+のチェックを入れないと使えません
+*/
+/*
+To show status bar message, please check the firefox option
+"content > Advanced JavaScript Settings > Change status bar text".
+*/
+	var s = 4;//回ホイールをページ末尾で下に回すとリロード
+	//Number of times wheel down on page bottom to reload.
+
+	var scrltop = true;//trueにするとリロード時にページ先頭に戻ります
+	//When this is true scrolls to top on reloading the page.
+
+	var t;
+	var n = 0;
+	window.addEventListener("wheel",function (e) {
+		window.clearTimeout(t);
+		var y = window.scrollY;
+		var ym = getScrollMaxY();
+		if (e.deltaY > 0 && y >= ym) {
+			n++;
+			window.status = "リロードぢから: " + parseInt(n * 100 / s) + "%";
+			if (n == s) {
+				tm();
+				location.reload();
+				return;
+			}
+			else
+				t = window.setTimeout(tm,1000);
+		}
+		else
+			tm();
+		return;
+	} ,false);
+	function tm() {
+		n = 0;
+		window.status="";
+		return;
+	}
+
+	/*  リロード時にページ先頭にスクロール  */
+	window.addEventListener("unload",function() {
+		if (scrltop){
+			window.scrollTo(0, 0);
+		}
+	} ,false);
+
+	/* ページ末尾判定 */
+	function getScrollMaxY() {
+		var innerh;
+		var yWithScroll;
+		if ( window.innerHeight ) {
+			 innerh = window.innerHeight;
+		}
+		else {
+			 innerh = document.body.clientHeight;
+		}
+		if ( window.innerHeight && window.scrollMaxY ) {
+			yWithScroll = window.innerHeight + window.scrollMaxY;
+		}
+		else if ( document.body.scrollHeight > document.body.offsetHeight ) {
+			yWithScroll = document.body.scrollHeight;
+		}
+		else {
+			yWithScroll = document.body.offsetHeight;
+		}
+		return yWithScroll - innerh;
+	}
+
+})();
